b8c474f56de688bc02e266f396348759d0bac99b
Eugene <evgeniys1@tut.by>
2018-10-04 19:57:19 +0300

    Update script.js

--- Lesson 1.1/script.js
+++ Lesson 1.1/script.js
@@ -2,12 +2,7 @@
 
 const num = 33721;
 
-let units = num%10, 
-    tens = Math.round((num%100)/10),
-    hundreds = Math.round((num%1000)/100),
-    thousands = Math.floor((num%10000)/1000),
-    tensOfThousands = Math.round(num/10000),
-    result = units * tens * hundreds * thousands * tensOfThousands,
+let result = num%10 * Math.round((num%100)/10) * Math.round((num%1000)/100) * Math.floor((num%10000)/1000) * Math.round(num/10000), 
     power = result ** 3;
 
 console.log(result);
