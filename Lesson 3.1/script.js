aa5495bcdec1fb9ac5806bbc487558aba2db4988
Ëugene <evgeniys1@tut.by>
2018-10-08 23:26:54 +0300

    Исправления

--- Lesson 3.1/script.js
+++ Lesson 3.1/script.js
@@ -14,23 +14,24 @@ console.log(str2);
 //Из получившейся строки вырезать слово “легким”, в этом же слове заменить 
 //2 последние буквы на букву “о”
 function replacedWord(str) {
-    let foundWord = str.match(/легким/ig);
-    let word = foundWord.toString();
-    let cutingPartOfWord = word.slice(0,word.length-2);
-    let resultingWord = cutingPartOfWord + 'оо';
-    alert(resultingWord);
+    // let foundWord = str.match(/легким/ig);
+    // let word = foundWord.toString();
+    // let cutingPartOfWord = word.slice(0,word.length-2);
+    // let resultingWord = cutingPartOfWord + 'оо';
+    let foundWord = str.match(/легким/ig).toString().slice(0,-3) + 'оо';
+    alert(foundWord);
 }
 replacedWord(str2);
 
 // Вывести в консоль квадратный корень из суммы кубов элементов массива arr 
 let arr = [20, 33, 1, "Человек", 2, 3];
 
-let Numbers = arr.filter(arr => typeof(arr) !== 'string');
+let numbers = arr.filter(arr => typeof(arr) !== 'string');
 
 function Cubs() {
     let cubs = 0;
-    for(let i = 0; i < Numbers.length; i++) {       
-        cubs += Numbers[i] ** 3; 
+    for(let i = 0; i < numbers.length; i++) {       
+        cubs += numbers[i] ** 3; 
     }   
     return Math.sqrt(cubs);
 }
@@ -38,13 +39,14 @@ let cubs = Cubs();
 console.log('Квадратный корень из суммы кубов элементов массива arr равен ' + cubs);
 
 //Создайте функцию, которая принимает 1 аргумент (название произвольное)
-    let testArg = 'dsfsdfsdsd';
+    let testArg = 'sadasd';
 
     function testFunc(arg) {
         console.log(typeof(arg) + " " + arg);
 
-        if( arg == 'number' || arg == 'underfind' || arg == '' || arg == null) {
+        if(arg == 'number' || arg == 'underfind' || arg == '' || arg == null || typeof(arg) == 'number') {
             alert('Вы ввели не строку!');
+            //callback();
         } else {
             arg.trim();
             if (arg.length > 50) {
@@ -53,4 +55,5 @@ console.log('Квадратный корень из суммы кубов эле
             console.log(arg);
         }
     }
+   
     testFunc(testArg);
