3546828ec3071b1b936e525180a59cb25d122ca6
Ëugene <evgeniys1@tut.by>
2018-10-08 21:56:32 +0300

    Задание 3.1, подправить ошибки с аргументами

--- Lesson 3.1/script.js
+++ Lesson 3.1/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Строка с заглавной буквы
+//Строка с заглавной буквы + замена всех "-" на пробелы
 let str = "урок-3-был слишком легким";
 
 function stringWithFirstSymbolToUpperCase(str) {
@@ -9,11 +9,11 @@ function stringWithFirstSymbolToUpperCase(str) {
     return str;
 }
 let str2 = stringWithFirstSymbolToUpperCase(str);
-//замените все "-" на пробелы
-//let str2 = str.replace(/-/g, ' ');
+
 console.log(str2);
 
-//Из получившейся строки вырезать слово “легким”, в этом же слове заменить 2 последние буквы на букву “о”
+//Из получившейся строки вырезать слово “легким”, в этом же слове заменить 
+//2 последние буквы на букву “о”
 function replacedWord(str) {
     let foundWord = str.match(/легким/ig);
     let word = foundWord.toString();
@@ -22,3 +22,36 @@ function replacedWord(str) {
     alert(resultingWord);
 }
 replacedWord(str2);
+
+// Вывести в консоль квадратный корень из суммы кубов элементов массива arr 
+let arr = [20, 33, 1, "Человек", 2, 3];
+
+let Numbers = arr.filter(arr => typeof(arr) !== 'string');
+
+function Cubs() {
+    let cubs = 0;
+    for(let i = 0; i < Numbers.length; i++) {       
+        cubs += Numbers[i] ** 3; 
+    }   
+    return Math.sqrt(cubs);
+}
+let cubs = Cubs();
+console.log('Квадратный корень из суммы кубов элементов массива arr равен ' + cubs);
+
+//Создайте функцию, которая принимает 1 аргумент (название произвольное)
+    //let testArg = prompt('Введите какую-нибудь строку');
+    let testArg = 12344;
+    function testFunc(arg) {
+        console.log(typeof(arg) + " " + arg);
+
+            if( arg == 'number' || arg == 'underfind' || arg == '' || arg == null) {
+                alert('Вы ввели не строку!');
+            } else {
+                arg.trim();
+                if (arg.length > 50) {
+                    arg = arg.substring(0, 50) + '...';
+                }
+                console.log(arg);
+            }
+        }
+    testFunc(testArg);
