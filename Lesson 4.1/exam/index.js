8dee3ae597300219df0ad3fd192d13d72c6314b5
Ëugene <evgeniys1@tut.by>
2018-10-09 23:53:38 +0300

    Задание 4.1 выполнено, тесты пройдены

--- Lesson 4.1/exam/index.js
+++ Lesson 4.1/exam/index.js
@@ -2,31 +2,62 @@ function getFriendlyNumbers(start, end) {
     if( (typeof(start) != 'number') || (typeof(end) != 'number') ||
         start == '' || start == null || end == '' || end == null || 
         start > end || start < 0 || ((start ^ 0) === 0) || ((end ^ 0) === 0)) {
-            console.log('bad choise');
+            //console.log('bad choise');
             return false;
     } else {
-        console.log('all right');
-        /*  Формула Сабита ибн Курра:
-            p = 3 * 2^(n-1) - 1,
-            q = 3 * 2^n - 1,
-            r = 9 * 2^(2n -1) - 1
-            если при n > 1 p, q и r будут простыми числами, 
-            то (2^n)pq и( 2^n)r - это будет пара дружественных чисел
-        */
-        /*
-            Метод Вальтера Бола:
-            A = a*u и B = a*s - дружественные числа, 
-            если u и p = u + s + 1 - простые числа. 
-            И ещё а не делится на p 
+        //console.log('all right');
 
-        */
+        let firstNumber,
+            secondNumber,
+            firstDividersSum,
+            secondDividersSum,
+            arr = [],
+            allDividers = {
+                    nDividers: [],
+                    mDividers: []
+                };
+            
+        //Первое число для сравнения
+        for(let i = start; i < end; i++) {
+            firstNumber = i;
+            //Поиск делителей 1 числа
+            for(let k = 1; k < i; k++) {
+                if( firstNumber%k == 0) {                   
+                    allDividers.nDividers.push(k);
+                }
+            }  
+            firstDividersSum = allDividers.nDividers.reduce( function(sum, current) {
+                return sum + current;
+              }, 0);
 
+            //Второе число для сравнения
+            for( let j = i+1; j <= end ; j++) {
+                secondNumber = j;
 
-        let arr = [];
-        return console.log(arr);
+                //Поиск делителей 2 числа
+                for(let k = 1; k < j; k++) {
+                    if( secondNumber%k == 0) {                   
+                        allDividers.mDividers.push(k);
+                    }
+                } 
+
+                secondDividersSum = allDividers.mDividers.reduce( function(sum, current) {
+                    return sum + current;
+                  }, 0);
+                allDividers.mDividers = [];
+               
+                if(firstDividersSum == secondNumber && secondDividersSum == firstNumber) {
+                    //console.log('Первое число: ' + firstNumber + ', Второе число: ' + secondNumber);
+                    arr.push([firstNumber, secondNumber]);    
+                } 
+            }
+            allDividers.nDividers = [];                 
+        }    
+        //console.log(arr); 
+        return arr;
     }
 }
-getFriendlyNumbers(1, 100);
+getFriendlyNumbers(1, 1);
 
 module.exports = {
     firstName: 'Name',
