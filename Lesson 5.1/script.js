5eff6b18e7ce58a4aff94b6919435dcfe58712f9
Ëugene <evgeniys1@tut.by>
2018-10-11 20:35:13 +0300

    задание 5.1

--- Lesson 5.1/script.js
+++ Lesson 5.1/script.js
@@ -2,16 +2,26 @@
 
 let months = ["01", "02", "03", "03", "05", "06", 
               "07", "08", "09", "10", "11", "12"],
-    date = [];
-
+    date = [],
+    day = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
 //09:59:59 30.05.2018
 let myDate = new Date(2018, 5, 30, 9, 59, 59);
-//  var fullDate = "'" + myDate.getDate() + " " + months[myDate.getMonth()] + 
-//                  " " + myDate.getFullYear() + "'";
 
 let fullDate = "'" + myDate.getHours() + ":" + myDate.getMinutes() + ":" +
                myDate.getSeconds() + " " + myDate.getDate() + ":" + 
-               myDate.getMonth() + ":" + myDate.getFullYear() + "'";
+               myDate.getMonth() + ":" + myDate.getFullYear() + ", " + day[myDate.getDay()] + "'";
 
-document.write(fullDate); // Сегодня: 18 Август 2015, Вторник
+document.write(fullDate); 
+console.log(myDate.getDay());
+//Напишите функцию, которая будет добавлять 0 перед днями и месяцами, 
+//которые состоят из одной цифры (из 1.6.2018 сделает 01.06.2018)
+function addZero() {
+    
+}
+
+//Напишите функцию, которая выводит на страницу текущий день 
+//недели на русском языке (реализацию выбираете сами)
+function dayOfTheWeek(day) {
+    
+}
