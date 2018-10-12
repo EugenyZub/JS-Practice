f3c7b3bbde75e8a69166d701a66f6ae68c424d30
Eugene <evgeniys1@tut.by>
2018-10-12 13:09:27 +0300

    Значительное уменьшение кода функции в задании 5.1

--- Lesson 5.1/script.js
+++ Lesson 5.1/script.js
@@ -8,7 +8,6 @@ let data1 = document.querySelector('#data1'),
     start = document.querySelector('#start'),
     end = document.querySelector('#result');
 
-    console.log(end);
 let fullDate = "'" + myDate.getHours() + ":" + myDate.getMinutes() + ":" +
                myDate.getSeconds() + " " + addZero(myDate.getDate(), myDate.getMonth()) + 
                ":" + myDate.getFullYear() + ", " + dayOfWeek(myDate.getDay()) + "'";
@@ -55,15 +54,9 @@ function dayOfWeek(day) {
 function datadDifference(data1, data2) {
 
     let firstDate,
+        time1,
+        time2,
         secondDate,
-        arrData1, 
-        arrData2,
-        date1Year,
-        date1Month,
-        date1Day,
-        date2Year,
-        date2Month,
-        date2Day,
         result = 0,
         negativeDate = 0; 
 
@@ -85,80 +78,31 @@ function datadDifference(data1, data2) {
             data1.value = '';
             data2.value = '';
 
-            //Массивы для нарезанных строк с датами
-            arrData1 = firstDate.split('-');
-            arrData2 = secondDate.split('-');
-
-            //Превращаем элементы массивов в отдельные числа
-            // date1Year = parseInt(arrData1[0]);
-            // date1Month = parseInt(arrData1[1]);
-            // date1Day = parseInt(arrData1[2]);
-            
-            // date2Year = parseInt(arrData2[0]);
-            // date2Month = parseInt(arrData2[1]);
-            // date2Day = parseInt(arrData2[2]);
-
-            //Массив из числе входящих в data1
-            arrData1.forEach(function(element, i) {
-                arrData1[i] = parseInt(arrData1[i]);
-            });
-
-            //Массив из числе входящих в data2
-            arrData2.forEach(function(element, i) {
-                arrData2[i] = parseInt(arrData2[i]);
-            });            
-               
-            //получаем милисекунды до 1970 года
-            let DATA1 = new Date(arrData1[0], arrData1[1]-1, arrData1[2]).getTime();
-            let DATA2 = new Date(arrData2[0], arrData2[1]-1, arrData2[2]).getTime();
+            //Преобразование даты в мс с 1970 года
+            time1 = new Date(Date.parse(firstDate));
+            time2 = new Date(Date.parse(secondDate));
 
             //Количество дней
-            if(DATA1 < DATA2) {
-                result = (DATA2 - DATA1) / 1000 / 60 / 60 / 24;
+            if(time1 < time2) {
+                result = (time2 - time1) / 1000 / 60 / 60 / 24;
                 negativeDate++;
-            } else if(DATA1 == DATA2) {
+            } else if(time1 == time2) {
                 result = 0;
-            } else if(DATA1 > DATA2) {
-                result = (DATA1 - DATA2) / 1000 / 60 / 60 / 24; 
+            } else if(time1 > time2) {
+                result = (time1 - time2) / 1000 / 60 / 60 / 24; 
                 negativeDate--;              
             }
             
             //Если дата/даты до 1970 года
-            if(DATA1 < 0) {
-                DATA1 = DATA1*(-1);
+            if(time1 < 0) {
+                time1 = time1*(-1);
             }
-            if(DATA2 < 0){
-                DATA2 -= DATA2*(-1);                
+            if(time2 < 0){
+                time2 -= time2*(-1);                
             }
             
-            //Вывод прямо на экран
-            // if(negativeDate > 0) {
-
-            //     if (result == 1) {
-            //         document.write('До этой даты вам осталось ждать ' + result + ' день!');                   
-            //     } else if(result == 2 || result == 3 || result == 4) {
-            //         document.write('До этой даты вам осталось ждать ' + result + ' дня!');   
-            //     } else {
-            //         document.write('До этой даты вам осталось ждать ' + result + ' дней!');    
-            //     }
-
-            // } else if(negativeDate < 0) {
-
-            //     if (result == 1) {
-            //         document.write('С этого момента прошёл ' + result + ' день!');
-            //     } else if(result == 2 || result == 3 || result == 4) {
-            //         document.write('С этого момента прошло ' + result + ' дня!');
-            //     } else {
-            //         document.write('С этого момента уже прошло ' + result + ' дней!');
-            //     }
-                
-            // } else {
-            //     document.write('Обе даты одинаковы');
-            // }
-
             //Вывод в инпут
             if(negativeDate > 0) {
-
                 if (result == 1) {
                     end.value = (result + ' день');                   
                 } else if(result == 2 || result == 3 || result == 4) {
@@ -166,24 +110,18 @@ function datadDifference(data1, data2) {
                 } else {
                     end.value = (result + ' дней');    
                 }
-
             } else if(negativeDate < 0) {
-
                 if (result == 1) {
                     end.value = (result + ' день');
                 } else if(result == 2 || result == 3 || result == 4) {
                     end.value = (result + ' дня');
                 } else {
                     end.value = (result + ' дней');
-                }
-                
+                }               
             } else {
                 end.value = ('Обе даты одинаковы');
             }
-
             negativeDate = 0;
         });
-
-    });
-    
+    });    
 }
---
+++ Lesson 6/js/main.js
@@ -0,0 +1,181 @@
+let start = document.getElementById('start'),
+
+    //Блоки правой части программы
+    budeget = document.getElementsByClassName('budget-value')[0],
+    daybudget = document.getElementsByClassName('daybudget-value')[0],
+    level = document.getElementsByClassName('level-value')[0],
+    expenses = document.getElementsByClassName('expenses-value')[0],
+    optionalexpenses = document.getElementsByClassName('optionalexpenses-value')[0],
+    income = document.getElementsByClassName('income-value')[0],
+    monthsavings = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavings = document.getElementsByClassName('yearsavings-value')[0],
+
+    //Обязательные расходы
+    expensesItem = document.querySelectorAll('.expenses-item'),
+
+    //Кнопки утвердить, рассчитать
+    approveExpenses = document.getElementsByTagName('button')[0],
+    approveOptionalExpenses = document.getElementsByTagName('button')[1],
+    calculate = document.getElementsByTagName('button')[2],
+    
+    //Необязательные расходы
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+    //Статьи возможного дохода
+    chooseIncome = document.querySelector('#income'), 
+
+    //Чекбокс
+    checksavings = document.querySelector('#savings'),
+
+    //Сумма
+    sum = document.querySelector('#sum'),
+
+    //Процент
+    percent = document.querySelector('#percent'), 
+
+    //Год
+    year = document.querySelector('.year-value'), 
+
+    //Месяц
+    month = document.querySelector('.month-value'),
+
+    //День
+    day = document.querySelector('.day-value'); 
+
+    let money, time;
+
+    start.addEventListener('click', function() {
+        time = prompt('Введите дату в формате YYYY-MM-DD', '');
+        money = +prompt('Ваш бюджет на месяц?', '');
+
+        while(isNaN(money) || money == '' || money == null) {
+            money = +prompt('Ваш бюджет на месяц?', '');
+        }
+
+        appData.finance = money;
+        appData.timeData = time;
+        budeget.textContent = money.toFixed();
+        yearsavings.value = new Date(Date.parse(time)).getFullYear();
+    });
+
+    let appData = {
+        finance: money,
+        timeData: new Date(time),
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: true,
+        moneyPerDay: 0,
+        chooseExpenses: function() {
+            for(let i = 0; i < 2; i++) {
+                answer1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+                answer2 = +prompt('Во сколько обойдется?', '');
+                
+                if ( (typeof(answer1)) === 'string' && (typeof(answer1))!= null && 
+                     (typeof(answer2)) != null && answer1 != '' && answer2 != '' && 
+                     answer1.length < 50 && isNaN(answer1) && !isNaN(answer2) ) {
+                    console.log('done');
+                    appData.expenses[answer1] = answer2;
+                } else {
+                    i--;
+                }
+            }
+        },
+        detectDayBudget: function(budget) {
+            appData.moneyPerDay = (budget/30).toFixed();
+        alert('Бюджет на 1 день: ' + appData.moneyPerDay);
+        },
+        detectLevel: function(moneyPerDay) {
+            if(moneyPerDay < 100) {
+                console.log('Минимальный уровень достатка');
+            } else if (moneyPerDay > 100 && moneyPerDay < 2000) {
+                console.log('Средний уровень достатка');
+            } else if (moneyPerDay > 2000) {
+                console.log('Высокий уровень достатка');
+            } else {
+                console.log('Произошла ошибка');
+            }
+        },
+        checkSavings: function() {
+            if(appData.savings == true) {
+                let save = +prompt('Какова сумма накоплений?', ''),
+                    percent = prompt('Под какой процент?', '');
+                
+                    appData.monthIncome = save/100/12*percent;
+                    alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+            }
+        },
+        chooseOptExpenses: function(optionalExpenses) {
+            for( let i = 0; i < 3; i++) {
+                let answer3 = prompt('Статья необязательных расходов?', '');
+                optionalExpenses[i] = answer3;
+            }
+            console.log(optionalExpenses);
+        },
+        chooseIncome: function() {            
+            let items = prompt('Что принесёт дополнительный доход? (Перечислите через запятую)', '');            
+    
+            while(typeof(items) != 'string' || items == null || items == '' | items == 'underfind') {
+                alert('Введите строку');
+                items = prompt('Что принесёт дополнительный доход? (Перечислите через запятую)', '');            
+            }
+            
+            appData.income = items.split(', ');
+            appData.income.push(prompt('Может что-то ещё?'));
+            appData.income.sort();
+            
+            document.write("Способы доп. заработка: <br>");
+            appData.income.forEach(function(item, i) {
+                document.write( (i+1) + '. ' + item.charAt(0).toUpperCase() + 
+                                item.slice(1) + '<br>');
+            });        
+        }
+    },  
+        answer1,
+        answer2;
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+//Вывод получившихся значений
+console.log(start);
+
+console.log('Блоки правой части программы');
+console.log(budeget);
+console.log(daybudget);
+console.log(level);
+console.log(expenses);
+console.log(optionalexpenses);
+console.log(income);
+console.log(monthsavings);
+console.log(yearsavings);
+
+console.log('Обязательные расходы');
+console.log(expensesItem);
+
+console.log('Кнопки');
+console.log(approveExpenses);
+console.log(approveOptionalExpenses);
+console.log(calculate);
+
+console.log('Необязательные расходы, Возможный доход');
+console.log(optionalExpensesItem);
+console.log(chooseIncome);
+
+console.log('Чекбокс, Сумма, Процент, Год, Месяц, День');
+console.log(checksavings);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
