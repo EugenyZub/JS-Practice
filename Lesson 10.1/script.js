392b8bbc89ad82176d812ec585ec411b089766c7
Ëugene <evgeniys1@tut.by>
2018-10-20 01:06:53 +0300

    Lesson 10.1 ready

--- Lesson 10.1/script.js
+++ Lesson 10.1/script.js
@@ -1,43 +1,49 @@
 'use strict';
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", () => {
 
-    function cursorPosition(pos, elem) {
-        elem.focus();
-        if (elem.setSelectionRange) {
-            elem.setSelectionRange(pos, pos);
-        } else if (elem.createTextRange) {
-            let range = elem.createTextRange();
-            range.collapse(true);
-            range.moveEnd("character", pos);
-            range.moveStart("character", pos);
-            range.select();
+    function cursorPosition(position, element) {
+        element.focus();
+    
+        if (element.setSelectionRange) {
+            element.setSelectionRange(position, position); //начальное и конечное положение выделения текста в inpute
+        } else if (element.createTextRange) {
+            let range = element.createTextRange();// Создаём объект TextRange 
+            range.collapse(true); //объединение конечных точек диапазона
+            range.moveEnd("character", position); //сдвиг конечных точек диапахона на единицу текста
+            range.moveStart("character", position);// в нашем случае это символ
+            range.select(); //для формирования выделения на основе содержимого объекта TextRange
         }
     }
 
-    function mask(event) {
-            let matrix = "+_ (___) ___ ____",
-                i = 0,
-                def = matrix.replace(/\D/g, ""),
-                val = this.value.replace(/\D/g, "");
-            if (def.length >= val.length) {
-                val = def;
-            }
-            this.value = matrix.replace(/./g, function(a) {
-                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
-            });
-            if (event.type == "blur") {
-                if (this.value.length == 2){
-                    this.value = "";
-                } 
-            } else {
-                cursorPosition(this.value.length, this);
-            }
+    //Маска
+    function mask(e) {
+        let mask = "+_ (___) ___-____", //Внешний вид маски
+            i = 0,
+            def = mask.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");// \D - любой нецифровой символ
+
+        if (def.length >= val.length) {
+            val = def;
         }
 
-            let input = document.querySelector("#tel");
-            input.addEventListener("input", mask, false);
-            input.addEventListener("focus", mask, false);
-            input.addEventListener("blur", mask, false);
+        this.value = mask.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+        });
+
+        //место ввода сохранится, если будет введено 2 и более символа
+        if (e.type == "blur") {
+            if (this.value.length == 2){
+                this.value = "";
+            } 
+        } else {
+            cursorPosition(this.value.length, this);
+        }
+    }
+
+    let input = document.querySelector("#tel");
+    input.addEventListener("input", mask);
+    input.addEventListener("focus", mask);
+    input.addEventListener("blur", mask);
         
 });
